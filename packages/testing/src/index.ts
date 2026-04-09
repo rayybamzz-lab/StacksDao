be30640d7faa931af41591270068671ba17afda1
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(testing): export package metadata

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,1 +1,7 @@
-export const testing = "testing";
+export const PACKAGE_NAME = '@stacksdao/testing';
+export const PACKAGE_SCOPE = 'testing';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
