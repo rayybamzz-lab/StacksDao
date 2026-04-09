348f8494ea59aaa214a82eec43537e8d961a74d5
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(nft): export package metadata

--- packages/nft/src/index.ts
+++ packages/nft/src/index.ts
@@ -1,1 +1,7 @@
-export const nft = "nft";
+export const PACKAGE_NAME = '@stacksdao/nft';
+export const PACKAGE_SCOPE = 'nft';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
