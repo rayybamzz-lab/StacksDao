45aa781bd1e1c90484653c520b6f72d29878dafa
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:53 +0100

    refactor(contracts): export package metadata

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,1 +1,7 @@
-export const contracts = "contracts";
+export const PACKAGE_NAME = '@stacksdao/contracts';
+export const PACKAGE_SCOPE = 'contracts';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
