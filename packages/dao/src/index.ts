d41921d92802bc35ca9d06cf06fd5d822d1a5f97
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:54 +0100

    refactor(dao): export package metadata

--- packages/dao/src/index.ts
+++ packages/dao/src/index.ts
@@ -1,1 +1,7 @@
-export const dao = "dao";
+export const PACKAGE_NAME = '@stacksdao/dao';
+export const PACKAGE_SCOPE = 'dao';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
