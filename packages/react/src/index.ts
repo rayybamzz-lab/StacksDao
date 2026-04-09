31a54cadbb0364be247946d08c9d769b0207fa09
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(react): export package metadata

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,7 @@
-export const react = "react";
+export const PACKAGE_NAME = '@stacksdao/react';
+export const PACKAGE_SCOPE = 'react';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
