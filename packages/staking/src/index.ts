e409d30ec4f734e8c41e52d520ac3e97d7334c8c
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(staking): export package metadata

--- packages/staking/src/index.ts
+++ packages/staking/src/index.ts
@@ -1,1 +1,7 @@
-export const staking = "staking";
+export const PACKAGE_NAME = '@stacksdao/staking';
+export const PACKAGE_SCOPE = 'staking';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
