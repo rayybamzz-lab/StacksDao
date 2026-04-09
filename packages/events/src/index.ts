043fd915f146bffdff78955e8ebd7c1f51b9f4e3
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:54 +0100

    refactor(events): export package metadata

--- packages/events/src/index.ts
+++ packages/events/src/index.ts
@@ -1,1 +1,7 @@
-export const events = "events";
+export const PACKAGE_NAME = '@stacksdao/events';
+export const PACKAGE_SCOPE = 'events';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
