9b4b4efded4d3f6ac3c638b2465f50a7b0041f41
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:54 +0100

    refactor(network): export package metadata

--- packages/network/src/index.ts
+++ packages/network/src/index.ts
@@ -1,1 +1,7 @@
-export const network = "network";
+export const PACKAGE_NAME = '@stacksdao/network';
+export const PACKAGE_SCOPE = 'network';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
