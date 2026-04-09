237fc47c8a6590130ba8bd6cfd0ee97850e1755b
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(utils): export package metadata

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,7 @@
-export const utils = "utils";
+export const PACKAGE_NAME = '@stacksdao/utils';
+export const PACKAGE_SCOPE = 'utils';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
