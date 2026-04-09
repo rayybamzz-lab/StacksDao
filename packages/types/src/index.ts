f511429778dc42e66dc6be95277242a492d27543
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(types): export package metadata

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,7 @@
-export const types = "types";
+export const PACKAGE_NAME = '@stacksdao/types';
+export const PACKAGE_SCOPE = 'types';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
