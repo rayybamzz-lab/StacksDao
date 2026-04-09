7b679aff454fd911f20d01f5b05a17837a45a677
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(queries): export package metadata

--- packages/queries/src/index.ts
+++ packages/queries/src/index.ts
@@ -1,1 +1,7 @@
-export const queries = "queries";
+export const PACKAGE_NAME = '@stacksdao/queries';
+export const PACKAGE_SCOPE = 'queries';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
