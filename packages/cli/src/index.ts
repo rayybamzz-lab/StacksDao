2000c5cffb48f10ad8376c8582dbe07db637eb0b
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:53 +0100

    refactor(cli): export package metadata

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,7 @@
-export const cli = "cli";
+export const PACKAGE_NAME = '@stacksdao/cli';
+export const PACKAGE_SCOPE = 'cli';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
