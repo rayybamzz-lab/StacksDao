986192f7b861cf542c2c5f3c9503ed53ba2ff326
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:54 +0100

    refactor(nextjs): export package metadata

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -1,1 +1,7 @@
-export const nextjs = "nextjs";
+export const PACKAGE_NAME = '@stacksdao/nextjs';
+export const PACKAGE_SCOPE = 'nextjs';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
