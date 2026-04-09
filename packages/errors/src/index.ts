ad593eadf9940288194e3438aba5ed6b8f801114
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:54 +0100

    refactor(errors): export package metadata

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,1 +1,7 @@
-export const errors = "errors";
+export const PACKAGE_NAME = '@stacksdao/errors';
+export const PACKAGE_SCOPE = 'errors';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
