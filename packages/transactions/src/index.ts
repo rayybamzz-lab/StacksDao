363141c15f610a7fe9b6ffef40b863fe6bc0b9ec
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:55 +0100

    refactor(transactions): export package metadata

--- packages/transactions/src/index.ts
+++ packages/transactions/src/index.ts
@@ -1,1 +1,7 @@
-export const transactions = "transactions";
+export const PACKAGE_NAME = '@stacksdao/transactions';
+export const PACKAGE_SCOPE = 'transactions';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
