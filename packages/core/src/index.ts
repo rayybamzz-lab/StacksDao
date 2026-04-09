1a6003feacf4a4f96490696bb8526684f62b7211
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:53 +0100

    refactor(core): export package metadata

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,7 @@
-export const core = "core";
+export const PACKAGE_NAME = '@stacksdao/core';
+export const PACKAGE_SCOPE = 'core';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
