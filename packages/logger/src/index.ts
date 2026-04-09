9a58533c7bac9b22269899bccc383825cc5ad0ea
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-09 12:34:54 +0100

    refactor(logger): export package metadata

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,1 +1,7 @@
-export const logger = "logger";
+export const PACKAGE_NAME = '@stacksdao/logger';
+export const PACKAGE_SCOPE = 'logger';
+
+export const packageMetadata = {
+  name: PACKAGE_NAME,
+  scope: PACKAGE_SCOPE,
+} as const;
