d547d952cb311881e88f7ccfc488d8238f23ca3b
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 17:46:10 +0100

    add set authorized minter args builder

--- packages/governance-token/src/index.ts
+++ packages/governance-token/src/index.ts
@@ -8,3 +8,4 @@ export const packageMetadata = {
 export function buildMintTokenArgs(amount: number, recipient: string): (string | number)[] { return [amount, recipient]; }
 export function buildTransferArgs(amount: number, sender: string, recipient: string): (string | number)[] { return [amount, sender, recipient]; }
 export function buildBurnArgs(amount: number, sender: string): (string | number)[] { return [amount, sender]; }
+export function buildSetAuthorizedMinterArgs(minter: string): (string | number)[] { return [minter]; }
