9b441020ed9e453465c150996d5f5a6b35bffed2
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 17:31:48 +0100

    add decimal place constants

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -6,3 +6,4 @@ export const packageMetadata = {
   scope: PACKAGE_SCOPE,
 } as const;
 export const CONTRACT_NAMES = { GOVERNANCE_TOKEN: 'governance-token-v2', STACKS_NFT: 'stacks-nft-v2', NFT_STAKING: 'nft-staking-v2', GOVERNANCE_DAO: 'governance-dao-v2' } as const;
+export const DECIMALS = { SDAO: 6, STX: 6, MICRO: 6 } as const;
