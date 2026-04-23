4f65b00280e4c1f75154c8346dc0e6f9ef31e054
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 17:32:10 +0100

    add base StacksDaoError class

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -5,3 +5,4 @@ export const packageMetadata = {
   name: PACKAGE_NAME,
   scope: PACKAGE_SCOPE,
 } as const;
+export class StacksDaoError extends Error { constructor(message: string) { super(message); this.name = 'StacksDaoError'; } }
