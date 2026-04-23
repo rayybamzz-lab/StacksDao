1dd54fefd113b7e3562d89ab2d932874cf27d5e5
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 17:33:39 +0100

    add user agent builder

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,3 +7,4 @@ export const packageMetadata = {
 } as const;
 export const APP_NAME = 'StacksDAO';
 export const APP_VERSION = '2.0.0';
+export function getUserAgent(): string { return APP_NAME + '/' + APP_VERSION; }
