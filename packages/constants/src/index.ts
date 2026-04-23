a26720b829096dfc43799ffd5f04d37fa7a7f1c8
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 17:31:48 +0100

    add block time constant

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -8,3 +8,4 @@ export const packageMetadata = {
 export const CONTRACT_NAMES = { GOVERNANCE_TOKEN: 'governance-token-v2', STACKS_NFT: 'stacks-nft-v2', NFT_STAKING: 'nft-staking-v2', GOVERNANCE_DAO: 'governance-dao-v2' } as const;
 export const DECIMALS = { SDAO: 6, STX: 6, MICRO: 6 } as const;
 export const GAS_LIMITS = { DEFAULT: 500000, MINT: 300000, STAKE: 400000, VOTE: 350000 } as const;
+export const BLOCK_TIME_SECONDS = 600;
