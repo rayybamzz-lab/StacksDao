5e4b66d94fd580702e813e10553cc29327ee2730
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 17:33:18 +0100

    add protocol event names

--- packages/events/src/index.ts
+++ packages/events/src/index.ts
@@ -5,3 +5,4 @@ export const packageMetadata = {
   name: PACKAGE_NAME,
   scope: PACKAGE_SCOPE,
 } as const;
+export const PROTOCOL_EVENTS = ['nft-staked', 'nft-unstaked', 'rewards-claimed', 'proposal-created', 'vote-cast', 'proposal-executed', 'authorized-minter-updated'] as const;
