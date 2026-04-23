9d4a13f1f67b16389cd1855683c6adae5e5868a2
rayybamzz-lab <rayybamzz@gmail.com>
2026-04-23 12:47:44 +0100

    test: drop the unused assert import

--- tests/stacksdao_test.ts
+++ tests/stacksdao_test.ts
@@ -1,5 +1,4 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.7.1/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
 // --------------------------------------------------------------------------
 // GOVERNANCE TOKEN (SIP-010) TESTS
